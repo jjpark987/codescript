1b2d37745a33ac9164b5b762568c9835309d406d
Jason Park <jjpark987@gmail.com>
2024-07-10 15:17:40 -0700

    complete urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .models import Category, Subcategory, Problem, Submission
+from .views import UserViewSet, CategoryViewSet, SubcategoryViewSet, ProblemViewSet, SubmissionViewSet
 
 router = DefaultRouter()
-# router.register(r'users', UserViewSet, basename='user')
-# router.register(r'search-queries', SearchQueryViewSet, basename='search-query')
-# router.register(r'products', ProductViewSet, basename='product')
-# router.register(r'reviews', ReviewViewSet, basename='review')
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'subcategories', SubcategoryViewSet, basename='subcategory')
+router.register(r'problems', ProblemViewSet, basename='problem')
+router.register(r'submissions', SubmissionViewSet, basename='submission')
 
 
 urlpatterns = [    
-    path('token/', TokenObtainPairView.as_view(), name='get_token'),    # POST
-    path('token/refresh/', TokenRefreshView.as_view(), name='refresh'),  # POST
+    path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='get_token'),    #POST
+    path('token/refresh/', TokenRefreshView.as_view(), name='refresh'), #POST
 ]
