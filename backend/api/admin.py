4c4246172e7047e8de4c744abfcde7f2f28d649d
Jason Park <jjpark987@gmail.com>
2024-08-20 17:58:55 -0700

    Initial commit for fastapi version

--- backend/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category, Subcategory, Problem, Example, Submission
-
-admin.site.register(Category)
-admin.site.register(Subcategory)
-admin.site.register(Problem)
-admin.site.register(Example)
-admin.site.register(Submission)
